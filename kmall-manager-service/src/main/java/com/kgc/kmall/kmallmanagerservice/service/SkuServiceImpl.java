ca6684580a691df4223d54d3777c638c6982efed
zhangjinlong <m18210244480@163.com>
2020-09-18 14:04:16 +0800

    利用redis优化商品详情页面

--- kmall-manager-service/src/main/java/com/kgc/kmall/kmallmanagerservice/service/SkuServiceImpl.java
+++ kmall-manager-service/src/main/java/com/kgc/kmall/kmallmanagerservice/service/SkuServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kgc.kmall.kmallmanagerservice.service;
 
+import com.alibaba.fastjson.JSON;
 import com.kgc.kmall.bean.PmsSkuAttrValue;
 import com.kgc.kmall.bean.PmsSkuImage;
 import com.kgc.kmall.bean.PmsSkuInfo;
@@ -9,10 +10,15 @@ import com.kgc.kmall.kmallmanagerservice.mapper.PmsSkuImageMapper;
 import com.kgc.kmall.kmallmanagerservice.mapper.PmsSkuInfoMapper;
 import com.kgc.kmall.kmallmanagerservice.mapper.PmsSkuSaleAttrValueMapper;
 import com.kgc.kmall.service.SkuService;
+import com.kgc.kmall.util.RedisUtil;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @Service
 public class SkuServiceImpl implements SkuService {
@@ -24,6 +30,8 @@ public class SkuServiceImpl implements SkuService {
     private PmsSkuImageMapper pmsSkuImageMapper;
     @Autowired
     private PmsSkuSaleAttrValueMapper pmsSkuSaleAttrValueMapper;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Override
     public String saveSkuInfo(PmsSkuInfo pmsSkuInfo) {
@@ -37,7 +45,53 @@ public class SkuServiceImpl implements SkuService {
 
     @Override
     public PmsSkuInfo selectBySkuId(Long id) {
-        return pmsSkuInfoMapper.selectByPrimaryKey(id);
+        Jedis jedis = redisUtil.getJedis();
+        String skuKey = "sku:" + id + ":info";
+        String skuInfoJson = jedis.get(skuKey);
+        if (skuInfoJson != null) {
+            if (skuInfoJson.equals("empty")) {
+                return null;
+            }
+            PmsSkuInfo pmsSkuInfo = JSON.parseObject(skuInfoJson, PmsSkuInfo.class);
+            jedis.close();
+            return pmsSkuInfo;
+        } else {
+            String skuLockKey = "sku:" + id + ":lock";
+            String skuLockValue= UUID.randomUUID().toString();
+            System.out.println(skuLockValue);
+            String lock = jedis.set(skuLockKey, skuLockValue, "NX", "PX",60*1000);
+
+            if (lock.equals("OK")) {
+                PmsSkuInfo pmsSkuInfo = pmsSkuInfoMapper.selectByPrimaryKey(id);
+                if (pmsSkuInfo == null) {
+                    jedis.setex(skuKey, 5 * 60 * 1000, "empty");
+                    return null;
+                }
+                //保存到redis
+                String skuInfoJsonStr = JSON.toJSONString(pmsSkuInfo);
+                Random random = new Random();
+                jedis.setex(skuKey, random.nextInt(10) * 60 * 1000 + 10000, skuInfoJsonStr);
+                String skuLockValue2 = jedis.get(skuLockKey);
+                if (skuLockValue2!=null&&skuLockValue2.equals(skuLockValue)){
+                    String script ="if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+                    jedis.eval(script, Collections.singletonList(skuLockKey),Collections.singletonList(skuLockValue));
+
+                }
+
+                jedis.close();
+                return pmsSkuInfo;
+            } else {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    return selectBySkuId(id);
+                }
+            }
+
+        }
+
     }
 
     @Override
--- kmall-manager-service/src/test/java/com/kgc/kmall/kmallmanagerservice/KmallManagerServiceApplicationTests.java
+++ kmall-manager-service/src/test/java/com/kgc/kmall/kmallmanagerservice/KmallManagerServiceApplicationTests.java
@@ -4,9 +4,12 @@ import com.kgc.kmall.bean.PmsBaseAttrInfo;
 import com.kgc.kmall.bean.PmsBaseCatalog1;
 import com.kgc.kmall.kmallmanagerservice.service.AttrServiceImpl;
 import com.kgc.kmall.kmallmanagerservice.service.CatalogServiceImpl;
+import com.kgc.kmall.util.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 import java.util.List;
 
@@ -33,5 +36,19 @@ class KmallManagerServiceApplicationTests {
             System.out.println(item);
         }
     }
+
+    @Autowired
+    RedisUtil redisUtil;
+    @Test
+    void contextLoadsRedis() {
+        try {
+            Jedis jedis = redisUtil.getJedis();
+            String name = jedis.get("name");
+            System.out.println(name);
+        }catch (JedisConnectionException e){
+            e.printStackTrace();
+        }
+    }
+
 
 }
